2afcfd236847cd8d0c5c850a56f7450d96b0dc73
Mathias Lieber <lieber@bitnetic.de>
2017-06-30 07:49:53 +0200

    Fix error in migration

--- src/backend/database/migrations/2017_06_27_000000_create_timetracking_tables.php
+++ src/backend/database/migrations/2017_06_27_000000_create_timetracking_tables.php
@@ -46,7 +46,7 @@ class CreateTimeTrackingTables extends Migration
             $table->integer('duration');
             $table->integer('project_id', 0);
             $table->string('ticket', 31);
-            $table->integer('activity_id', 31);
+            $table->integer('activity_id');
             $table->string('description', 255);
             $table->timestamps();
         });
